1ff1a7e75ced1ac51534e66f27268bb6e6cee2d2
Alex Arias <aarias077@gmail.com>
2025-12-08 13:09:07 -0600

    created and connected db

--- Back-End/japanHatsu_project/japanHatsu_project/settings.py
+++ Back-End/japanHatsu_project/japanHatsu_project/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'japanHatsu_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'japanHatsu_db',
     }
 }
 
